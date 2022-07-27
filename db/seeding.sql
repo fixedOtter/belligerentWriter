80ccbe4398274555e5e342c18afeeccd59354802
fixedOtter <fo1152rc@go.minnstate.edu>
2022-07-27 14:35:51 -0500

    started work with SQL database

---
+++ db/seeding.sql
@@ -0,0 +1,11 @@
+USE notes_data;
+
+INSERT INTO notes (title, words) VALUES
+  ('title', 'things'),
+  ('dream', 'dreamjournal'),
+  ('thoughts', 'so many thoughts in my little head'),
+  ('awesome', 'you are ;)'),
+  ('what', 'yex eacactly this is a what'),
+  ('the item', 'ii have on respecto'),
+  ('colors', 'my favorite is pink right now'),
+  ('p', 'yes');
